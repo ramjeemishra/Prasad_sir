e850dd09b0761b96f3aa9f037fecc011880f0cc7
Ramjee Mishra <131847922+ramjeemishra@users.noreply.github.com>
2025-10-27 23:14:52 +0530

    Add files via upload

--- day1.py
+++ day1.py
@@ -21,3 +21,17 @@ name = input("enter ur name:-")
 age = int(input("enter ur age:-"))
 print("your name is:-",name, "ur age is:-",age)
 
+# question given on lisa
+# Exercise 1: Mailing Address
+name = "prasad sir"
+street_address = "123 Main Street, taj hotel 4B"
+city_state_zip = "uk, CA 90210"
+country = "mars"
+
+print(name)
+print(street_address)
+print(city_state_zip)
+print(country)
+
+# OR, using a single print statement:
+print(f"{name}\n{street_address}\n{city_state_zip}\n{country}")
